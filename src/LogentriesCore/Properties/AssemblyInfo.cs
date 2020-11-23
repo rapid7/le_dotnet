031e62aca669849756fc297b85970cd4506b83cc
Szymon Bialkowski <szymon_bialkowski@rapid7.com>
2020-11-23 14:53:06 +0000

    LOG-15585 Make it build

--- src/LogentriesCore/Properties/AssemblyInfo.cs
+++ src/LogentriesCore/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LogentriesCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Logentries logging core library for dotnet support.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Logentries")]
 [assembly: AssemblyProduct("LogentriesCore")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
